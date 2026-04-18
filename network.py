a84144baac99530ffdac83076301d21d4ae76e1d
Mike Gidaro <mgidaro@verizon.net>
2026-04-18 19:28:14 -0400

    added multiple models

--- network.py
+++ network.py
@@ -28,38 +28,92 @@ from data_loading import RegressionTaskData
 
 class CNNRegression(nn.Module):
     #Need to add layers to match VGG-11
-    def __init__(self, image_size: Tuple[int, int, int] = (1, 640, 470)):
+    def __init__(self, image_size: Tuple[int, int, int], m_num: int):
         super(CNNRegression, self).__init__()
         self.image_size = image_size
-        self.conv1 = nn.Conv2d(in_channels=self.image_size[0], out_channels=4, kernel_size=3, stride=1, padding=1)
-        #self.batch1 = nn.BatchNorm2d(num_features=4)
-        #self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(in_channels=4, out_channels=16, kernel_size=3, stride=1, padding=1)
-        #self.batch2 = nn.BatchNorm2d(num_features=16)
-        #self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        #self.conv3 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=1, padding=1)
-        #self.batch3 = nn.BatchNorm2d(num_features=16)
-        #self.conv4 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, stride=1, padding=1)
-        #self.batch4 = nn.BatchNorm2d(num_features=16)
-        ##self.pool4  = nn.MaxPool2d(kernel_size=2, stride=2)
-        #self.conv5 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
-        #self.batch5 = nn.BatchNorm2d(num_features=32)
-        #self.conv6 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1)
-        ##self.batch6 = nn.BatchNorm2d(num_features=32)
-        ##self.pool6 = nn.MaxPool2d(kernel_size=2, stride=2)
-        #self.conv7 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1)
-        ##self.batch7 = nn.BatchNorm2d(num_features=32)
-        #self.conv8 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1)
-        #self.batch8 = nn.BatchNorm2d(num_features=32)
-        #self.pool8 = nn.MaxPool2d(kernel_size=2, stride=2)
+        if m_num == 0:
         
-        #self.avg = nn.AdaptiveAvgPool2d(output_size=7)
-        
-        self.linear_line_size = int(16*(image_size[1]//4)*(image_size[2]//4))
-        self.fc1 = nn.Linear(in_features=self.linear_line_size, out_features=128)    
-        #self.fc2 = nn.Linear(in_features=128, out_features=128)
-        self.fc3 = nn.Linear(in_features=128, out_features=1)
+            self.conv1 = nn.Conv2d(in_channels=self.image_size[0], out_channels=4, kernel_size=3, stride=1, padding=1)
+            #self.batch1 = nn.BatchNorm2d(num_features=4)
+            self.conv2 = nn.Conv2d(in_channels=4, out_channels=16, kernel_size=3, stride=1, padding=1)
+            #self.batch2 = nn.BatchNorm2d(num_features=16)
+            
+            self.linear_line_size = int(16*(image_size[1]//4)*(image_size[2]//4))
+            self.fc1 = nn.Linear(in_features=self.linear_line_size, out_features=128)    
+            self.fc2 = nn.Linear(in_features=128, out_features=1)
 
+        elif m_num == 1:
+            self.conv1 = nn.Conv2d(in_channels=self.image_size[0], out_channels=4, kernel_size=3, stride=1, padding=1)
+            self.conv2 = nn.Conv2d(in_channels=4, out_channels=4, kernel_size=3, stride=1, padding=1)
+            self.conv3 = nn.Conv2d(in_channels=4, out_channels=16, kernel_size=3, stride=1, padding=1)
+            
+            self.linear_line_size = int(16*(image_size[1]//8)*(image_size[2]//8))
+            self.fc1 = nn.Linear(in_features=self.linear_line_size, out_features=128)    
+            self.fc2 = nn.Linear(in_features=128, out_features=1)
+
+        elif m_num == 2:
+            self.conv1 = nn.Conv2d(in_channels=self.image_size[0], out_channels=16, kernel_size=3, stride=1, padding=1)
+            
+            self.linear_line_size = int(16*(image_size[1]//2)*(image_size[2]//2))
+            self.fc1 = nn.Linear(in_features=self.linear_line_size, out_features=128)    
+            self.fc2 = nn.Linear(in_features=128, out_features=1)
+
+        elif m_num == 3:
+            self.conv1 = nn.Conv2d(in_channels=self.image_size[0], out_channels=2, kernel_size=3, stride=1, padding=1)
+            self.conv2 = nn.Conv2d(in_channels=2, out_channels=8, kernel_size=3, stride=1, padding=1)
+            
+            self.linear_line_size = int(8*(image_size[1]//4)*(image_size[2]//4))
+            self.fc1 = nn.Linear(in_features=self.linear_line_size, out_features=128)    
+            self.fc2 = nn.Linear(in_features=128, out_features=1)
+
+        elif m_num == 4:
+            self.conv1 = nn.Conv2d(in_channels=self.image_size[0], out_channels=8, kernel_size=3, stride=1, padding=1)
+            self.conv2 = nn.Conv2d(in_channels=8, out_channels=32, kernel_size=3, stride=1, padding=1)
+            
+            self.linear_line_size = int(32*(image_size[1]//4)*(image_size[2]//4))
+            self.fc1 = nn.Linear(in_features=self.linear_line_size, out_features=128)    
+            self.fc2 = nn.Linear(in_features=128, out_features=1)
+
+        elif m_num == 5:
+            self.conv1 = nn.Conv2d(in_channels=self.image_size[0], out_channels=4, kernel_size=3, stride=1, padding=1)
+            self.conv2 = nn.Conv2d(in_channels=4, out_channels=16, kernel_size=3, stride=1, padding=1)
+            
+            self.linear_line_size = int(16*(image_size[1]//4)*(image_size[2]//4))
+            self.fc1 = nn.Linear(in_features=self.linear_line_size, out_features=1)    
+
+        elif m_num == 6:
+            self.conv1 = nn.Conv2d(in_channels=self.image_size[0], out_channels=4, kernel_size=3, stride=1, padding=1)
+            self.conv2 = nn.Conv2d(in_channels=4, out_channels=16, kernel_size=3, stride=1, padding=1)
+            
+            self.linear_line_size = int(16*(image_size[1]//4)*(image_size[2]//4))
+            self.fc1 = nn.Linear(in_features=self.linear_line_size, out_features=1024)
+            self.fc2 = nn.Linear(in_features=1024, out_features=256)
+            self.fc3 = nn.Linear(in_features=256, out_features=1)
+
+        elif m_num == 7:
+            self.conv1 = nn.Conv2d(in_channels=self.image_size[0], out_channels=4, kernel_size=3, stride=1, padding=1)
+            self.batch1 = nn.BatchNorm2d(num_features=4)
+            self.conv2 = nn.Conv2d(in_channels=4, out_channels=16, kernel_size=3, stride=1, padding=1)
+            self.batch2 = nn.BatchNorm2d(num_features=16)
+            
+            self.linear_line_size = int(16*(image_size[1]//4)*(image_size[2]//4))
+            self.fc1 = nn.Linear(in_features=self.linear_line_size, out_features=128)    
+            self.batch3 = nn.BatchNorm1d(num_features=128)
+            self.fc2 = nn.Linear(in_features=128, out_features=1)
+
+        elif m_num == 8:
+            self.conv1 = nn.Conv2d(in_channels=self.image_size[0], out_channels=4, kernel_size=3, stride=1, padding=1)
+            self.conv2 = nn.Conv2d(in_channels=4, out_channels=16, kernel_size=3, stride=1, padding=1)
+            
+            self.linear_line_size = int(16*(image_size[1]//4)*(image_size[2]//4))
+            self.fc1 = nn.Linear(in_features=self.linear_line_size, out_features=128)    
+            self.fc2 = nn.Linear(in_features=128, out_features=1)
+
+        #elif m_num == 9:
+            #the best of them all
+
+        else:
+            raise NameError('model doesnt exist!')
         
     def forward(self, x):
         """
@@ -69,65 +123,151 @@ class CNNRegression(nn.Module):
         the image size changes and you want to check that the network layers are 
         still the correct shape.
         """
+        if m_num == 0:
+            x = self.conv1(x)
+            # print('Size of tensor after each layer')
+            # print(f'conv1 {x.size()}')
+            #x = self.batch1(x)
+            # print(f'batch1 {x.size()}')
+            x = nn.functional.relu(x)
+            # print(f'relu1 {x.size()}')
+            x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
+            # print(f'pool1 {x.size()}')
+            x = self.conv2(x)
+            # print(f'conv2 {x.size()}')
+            #x = self.batch2(x)
+            x = nn.functional.relu(x)
+            # print(f'relu2 {x.size()}')
+            x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
+            # print(f'pool2 {x.size()}')
 
-        x = self.conv1(x)
-        # print('Size of tensor after each layer')
-        # print(f'conv1 {x.size()}')
-        #x = self.batch1(x)
-        # print(f'batch1 {x.size()}')
-        x = nn.functional.relu(x)
-        # print(f'relu1 {x.size()}')
-        x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
-        # print(f'pool1 {x.size()}')
-        x = self.conv2(x)
-        # print(f'conv2 {x.size()}')
-        #x = self.batch2(x)
-        x = nn.functional.relu(x)
-        # print(f'relu2 {x.size()}')
-        x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
-        # print(f'pool2 {x.size()}')
-        #x = self.conv3(x)
-        #x = self.batch3(x)
-        #x = nn.functional.relu(x)
-        #x = self.conv4(x)
-        #x = self.batch4(x)
-        #x = nn.functional.relu(x)
-        #x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
-        #x = self.conv5(x)
-        #x = self.batch5(x)
-        #x = nn.functional.relu(x)
-        #x = self.conv6(x)
-        #x = self.batch6(x)
-        #x = nn.functional.relu(x)
-        #x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
-        #x = self.conv7(x)
-        ##x = self.batch7(x)
-        #x = nn.functional.relu(x)
-        #x = self.conv8(x)
-        ##x = self.batch8(x)
-        #x = nn.functional.relu(x)
-        #x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
 
-        #x = self.avg(x)
+            x = x.view(-1, self.linear_line_size)
+            # print(f'view1 {x.size()}')
+            #x = torch.flatten(x,1)
+            x = self.fc1(x)
+            # print(f'fc1 {x.size()}')
+            x = nn.functional.relu(x)
+            # print(f'relu2 {x.size()}')
+            #x = nn.functional.dropout(x, p=0.5, inplace=False)
+            x = self.fc2(x)
 
-        x = x.view(-1, self.linear_line_size)
-        # print(f'view1 {x.size()}')
-        #x = torch.flatten(x,1)
-        x = self.fc1(x)
-        # print(f'fc1 {x.size()}')
-        x = nn.functional.relu(x)
-        # print(f'relu2 {x.size()}')
-        #x = nn.functional.dropout(x, p=0.5, inplace=False)
-        #x = self.fc2(x)
-        # print(f'fc2 {x.size()}')
-        #x = nn.functional.relu(x)
-        #x = nn.functional.dropout(x, p=0.5, inplace=False)
-        x = self.fc3(x)
+        elif m_num == 1:
+            x = self.conv1(x)
+            x = nn.functional.relu(x)
+            x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
+            x = self.conv2(x)
+            x = nn.functional.relu(x)
+            x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
+            x = self.conv3(x)
+            x = nn.functional.relu(x)
+            x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
+
+
+            x = x.view(-1, self.linear_line_size)
+            x = self.fc1(x)
+            x = nn.functional.relu(x)
+            x = self.fc2(x)
+
+        elif m_num == 2:
+            x = self.conv1(x)
+            x = nn.functional.relu(x)
+            x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
+            
+            x = x.view(-1, self.linear_line_size)
+            x = self.fc1(x)
+            x = nn.functional.relu(x)
+            x = self.fc2(x)
+
+        elif m_num == 3:
+            x = self.conv1(x)
+            x = nn.functional.relu(x)
+            x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
+            x = self.conv2(x)
+            x = nn.functional.relu(x)
+            x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
+            
+            x = x.view(-1, self.linear_line_size)
+            x = self.fc1(x)
+            x = nn.functional.relu(x)
+            x = self.fc2(x)
+
+        elif m_num == 4:
+            x = self.conv1(x)
+            x = nn.functional.relu(x)
+            x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
+            x = self.conv2(x)
+            x = nn.functional.relu(x)
+            x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
+            
+            x = x.view(-1, self.linear_line_size)
+            x = self.fc1(x)
+            x = nn.functional.relu(x)
+            x = self.fc2(x)
+
+        elif m_num == 5:
+            x = self.conv1(x)
+            x = nn.functional.relu(x)
+            x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
+            x = self.conv2(x)
+            x = nn.functional.relu(x)
+            x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
+            
+            x = x.view(-1, self.linear_line_size)
+            x = self.fc1(x)
+
+        elif m_num == 6:
+            x = self.conv1(x)
+            x = nn.functional.relu(x)
+            x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
+            x = self.conv2(x)
+            x = nn.functional.relu(x)
+            x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
+            
+            x = x.view(-1, self.linear_line_size)
+            x = self.fc1(x)
+            x = nn.functional.relu(x)
+            x = self.fc2(x)
+            x = nn.functional.relu(x)
+            x = self.fc3(x)
+
+        elif m_num == 7:
+            x = self.conv1(x)
+            x = self.batch1(x)
+            x = nn.functional.relu(x)
+            x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
+            x = self.conv2(x)
+            x = self.batch2(x)
+            x = nn.functional.relu(x)
+            x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
+            
+            x = x.view(-1, self.linear_line_size)
+            x = self.fc1(x)
+            x = self.batch3(x)
+            x = nn.functional.relu(x)
+            x = self.fc2(x)
+
+        elif m_num == 8:
+            x = self.conv1(x)
+            x = nn.functional.relu(x)
+            x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
+            x = self.conv2(x)
+            x = nn.functional.relu(x)
+            x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
+            
+            x = x.view(-1, self.linear_line_size)
+            x = self.fc1(x)
+            x = nn.functional.relu(x)
+            x = nn.functional.dropout(x, p=0.5, inplace=False)
+            x = self.fc2(x)
+
+        #elif m_num == 9:
+            #the best of them all
 
         return x
     
 
-def train_network(device, n_epochs: int = 10, image_size: Tuple[int, int, int] = (3, 640, 470)):
+def train_network(device, n_epochs: int, image_size: Tuple[int, int, int], image_folder_path: str, m_num: int):
     """
     This trains the network for a set number of epochs.
     """
@@ -135,14 +275,14 @@ def train_network(device, n_epochs: int = 10, image_size: Tuple[int, int, int] =
     #grayscale = True
     #else:
     #    grayscale = False
-    assert image_size[1] == 640, 'size 1 wasnt 640'
-    assert image_size[2] == 470, 'size 2 wasnt 470'
+    assert image_size[1] == 280, 'size 1 wasnt 280'
+    assert image_size[2] == 150, 'size 2 wasnt 150'
     print(f'training')
     #resize_size = image_size[1]
-    regression_task = RegressionTaskData(grayscale=True, image_folder_path="C:/Users/Mike/Documents/imgAnalysis/recordings/US_frames/US_version4")#, resize_size=resize_size)
+    regression_task = RegressionTaskData(grayscale=True, image_folder_path=image_folder_path)#, resize_size=resize_size)
 
     # Define the model, loss function, and optimizer
-    model = CNNRegression(image_size=image_size)
+    model = CNNRegression(image_size=image_size, m_num=m_num)
     model.to(device)
     print(model)
     criterion = nn.MSELoss()
@@ -180,16 +320,16 @@ def save_model(model, filename='3_100_100.pth'):
     torch.save(model.state_dict(), filename)
 
 
-def load_model(image_size=(3, 640, 470), filename='3_100_100.pth'):
+def load_model(image_size, filename, m_num):
     """
     Load the model from the saved state dictionary.
     """
-    model = CNNRegression(image_size)
+    model = CNNRegression(image_size, m_num=m_num)
     model.load_state_dict(torch.load(filename))
     return model
 
 
-def evaluate_network(model, device, image_size: Tuple[int, int, int] = (3, 640, 470)):
+def evaluate_network(model, device, image_size: Tuple[int, int, int] = (3, 640, 470), image_folder_path: str = "C:/Users/Mike/Documents/imgAnalysis/recordings/US_frames/US_version6"):
     """
     This evaluates the network on the test data.
     """
@@ -197,10 +337,10 @@ def evaluate_network(model, device, image_size: Tuple[int, int, int] = (3, 640, 
     #    grayscale = True
     #else:
     #    grayscale = False
-    assert image_size[1] == 640, 'eval: size 1 wasnt 640'
-    assert image_size[2] == 470, 'eval: size 2 wasnt 470'
+    assert image_size[1] == 280, 'size 1 wasnt 280'
+    assert image_size[2] == 150, 'size 2 wasnt 150'
     #resize_size = image_size[1]
-    regression_task = RegressionTaskData(grayscale=True, image_folder_path="C:/Users/Mike/Documents/imgAnalysis/recordings/US_frames/US_version4")#, resize_size=resize_size)
+    regression_task = RegressionTaskData(grayscale=True, image_folder_path=image_folder_path)#, resize_size=resize_size)
     criterion = nn.MSELoss()
 
     # Evaluate the model on the test data
@@ -242,10 +382,12 @@ def evaluate_network(model, device, image_size: Tuple[int, int, int] = (3, 640, 
 
 if __name__ == '__main__':
     
-    num_epochs = 50
+    m_num = 0
+    num_epochs = 20
     #dont forget
-    filename = 'C:/Users/Mike/Documents/imgAnalysis/models/oneDtutorial.pth'
-    image_size: Tuple[int, int, int] = (1, 640, 470)
+    filename = f'C:/Users/Mike/Documents/imgAnalysis/models/m{m_num}.pth', 
+    image_folder_path = "C:/Users/Mike/Documents/imgAnalysis/recordings/US_frames/US_version6"
+    image_size: Tuple[int, int, int] = (1, 280, 150)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f'Using device: {device}')
@@ -253,11 +395,10 @@ if __name__ == '__main__':
 
     
     # Train the model
-    #model = train_network(device, n_epochs=num_epochs, image_size=image_size)
+    model = train_network(device, n_epochs=num_epochs, image_size=image_size, image_folder_path=image_folder_path, m_num=m_num)
 
     # Save the model
-    #filename = f'{image_size[0]}_{image_size[1]}_{image_size[2]}.pth'
-    #save_model(model, filename=filename)
+    save_model(model, filename=filename)
 
     # Load the model
     model = load_model(image_size=image_size, filename=filename)
@@ -266,5 +407,5 @@ if __name__ == '__main__':
     '''print(model)'''
 
     # Evaluate the model
-    evaluate_network(model, device, image_size=image_size)
+    evaluate_network(model, device, image_size=image_size, image_folder_path=image_folder_path, m_num=m_num)
     
